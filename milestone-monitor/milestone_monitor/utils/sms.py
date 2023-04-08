db1aa18de9fe4e27271bb341593a4da7dcc842d1
Shiva Menta <shiva.123.menta@gmail.com>
2023-04-08 15:39:23 -0400

    Set up Docker environment

---
+++ milestone-monitor/milestone_monitor/utils/sms.py
@@ -0,0 +1,23 @@
+from twilio.rest import Client
+import os
+import sys
+
+from backend.settings import (
+    twilio_account_sid,
+    twilio_auth,
+    messaging_service_sid
+)
+
+client = Client(twilio_account_sid, twilio_auth)
+
+def send_sms(number, body):
+    """
+    Send SMS message to input number with input body using Twilio messaging.
+    """
+    message = client.messages.create(
+        messaging_service_sid=messaging_service_sid,
+        body=body,
+        to=number
+    )
+    return message.sid
+
